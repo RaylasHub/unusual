8fa5dee01d2188115c1ca0d62517551e91cfaa84
RaylasHub <88529561+RaylasHub@users.noreply.github.com>
2021-08-06 10:05:58 +0300

    Update Rayla's Hub.lua

--- Rayla's Hub.lua
+++ Rayla's Hub.lua
@@ -517,7 +517,7 @@ local Flux = loadstring(game:HttpGet("https://raw.githubusercontent.com/Screamer
 UI WINDOWS
 ]]
 
-local FluxWindow = Flux:Window("dont ban me mui", "", Color3.fromRGB(200, 200, 200), Enum.KeyCode.RightAlt)
+local FluxWindow = Flux:Window("talut sbr carry", "", Color3.fromRGB(200, 200, 200), Enum.KeyCode.RightAlt)
 
 --[[
 UI TABS
